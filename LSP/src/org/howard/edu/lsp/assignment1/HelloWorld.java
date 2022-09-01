93cca8df8aa4f3c25dcbe56413758ae410792663
kuiraedwards <kuiraedwards@192.168.0.207>
2022-09-01 16:54:45 -0700

    This is my commit for assignment #1

---
+++ LSP/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,14 @@
+package org.howard.edu.lsp.assignment1;
+
+/**
+ * Kuira Edwards
+ * @author kuiraedwards
+ *
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+
+}
