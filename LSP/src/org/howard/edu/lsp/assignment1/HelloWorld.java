ee7d5eff07e6c549f8598fa429318107e3feb311
kuiraedwards <kuiraedwards@192.168.0.207>
2022-09-01 17:07:29 -0700

    Assignment #1

--- LSP/src/org/howard/edu/lsp/assignment1/HelloWorld.java
+++ LSP/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -8,7 +8,7 @@ package org.howard.edu.lsp.assignment1;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello Large Scale Programming!!");
 	}
 
 }
